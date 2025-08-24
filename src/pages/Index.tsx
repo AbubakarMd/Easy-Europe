bfc4b79ca10cb48f3775c1ef73e4299457f3c18d
Bushra parween <bushraparween2017@gmail.com>
2025-08-24 17:17:13 +0530

    Initial commit

---
+++ src/pages/Index.tsx
@@ -0,0 +1,27 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Services from "@/components/Services";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import Blog from "@/components/Blog";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <main>
+        <Hero />
+        <About />
+        <Services />
+        <WhyChooseUs />
+        <Blog />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
